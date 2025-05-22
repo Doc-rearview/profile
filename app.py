f430917c2504bc7916921d0f5ae33b91d20b2dae
Akshit Sharma <162539818+Doc-rearview@users.noreply.github.com>
2025-05-22 13:46:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def download_cv():
     return send_from_directory('static', 'pdf/cv.pdf', as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0',debug=True)
